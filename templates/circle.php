9081264f939b388be2a73591c4ffc6233bdc0432
Jorge Vale <jorge-vale1@hotmail.com>
2017-03-04 17:34:50 -0800

    feed and circle

--- templates/circle.php
+++ templates/circle.php
@@ -1,5 +1,5 @@
 <div class="container">
-    <div class="row">circle.php
+    <div class="row">
         <div id="left_sidebar" class="col-xs-2 col-sm-2">
             <div class="row">
                 <button onclick="open_left_sidebar()" class="btn btn-default btn-block hidden-sm hidden-md hidden-lg" id="left_sidebar_btn"
@@ -85,7 +85,103 @@
         </div>
 
         <div id="center" class="col-xs-12 col-sm-8 col-sm-pull-2">
+			<div class="row">
+				<div class="col-xs-8 col-sm-8"<p><a href="#" class="btn btn-info btn-lg">
+					<span class="glyphicon glyphicon-plus"></span>Add to group</a><input type="text"/></p>
+				</div>
+				<div class="col-xs-4 col-sm-4"<p><a href="#" class="btn btn-info btn-lg">
+					<span class="glyphicon glyphicon-remove"></span>Leave group</a></p>
+				</div>
             <div id="post1" class="row">
+                <div class="col-xs-1"><img src="../res/user.png" class="img-responsive"style="width:60px"></div>
+                <div class="col-xs-11">
+                    <h4>User1 <small><i>Posted on February 19, 2016</i></small></h4>
+                </div>
+                <div class="col-xs-12 col-sm-6">
+                    <p>Cras sit amet nibh libero, in gravida nulla.
+                    <p>Nulla vel metus scelerisque ante sollicitudin.
+                    Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+                    Fusce condimentum nunc ac nisi vulputate fringilla.
+                    Donec lacinia congue felis in faucibus.</p>
+                </div>
+                <div class="col-xs-12 col-sm-6"><img src="../res/book.png" class="img-responsive"></div>
+                <div class="col-xs-1">
+                    <span class="glyphicon glyphicon-arrow-up"></span>
+                </div>
+                <div class="col-xs-9">
+                    <h3 class="text-center">Comments</h3>
+                </div>
+                <div class="col-xs-1">
+                    <span class="glyphicon glyphicon-arrow-down"></span>
+                </div>
+                <div class="col-xs-12">
+                <div class="media">
+                    <div class="media-left">
+                        <img src="../res/user.png" class="media-object" style="width:60px">
+                    </div>
+                    <div class="media-body">
+                        <h4 class="media-heading">User2 <small><i>19/02/2016 22:30</i></small></h4>
+                        <p>És uma merda</p>
+                    </div>
+                </div>
+                    <div class="media">
+                        <div class="media-left">
+                            <img src="../res/user.png" class="media-object" style="width:60px">
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">User2 <small><i>19/02/2016 22:46</i></small></h4>
+                            <p>Mesmo Caralho</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+			<br>
+			<br>
+			<div id="post2" class="row">
+                <div class="col-xs-1"><img src="../res/user.png" class="img-responsive"style="width:60px"></div>
+                <div class="col-xs-11">
+                    <h4>User1 <small><i>Posted on February 19, 2016</i></small></h4>
+                </div>
+                <div class="col-xs-12 col-sm-12">
+                    <p>Cras sit amet nibh libero, in gravida nulla.
+                    <p>Nulla vel metus scelerisque ante sollicitudin.
+                    Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+                    Fusce condimentum nunc ac nisi vulputate fringilla.
+                    Donec lacinia congue felis in faucibus.</p>
+                </div>
+                <div class="col-xs-1">
+                    <span class="glyphicon glyphicon-arrow-up"></span>
+                </div>
+                <div class="col-xs-9">
+                    <h3 class="text-center">Comments</h3>
+                </div>
+                <div class="col-xs-1">
+                    <span class="glyphicon glyphicon-arrow-down"></span>
+                </div>
+                <div class="col-xs-12">
+                <div class="media">
+                    <div class="media-left">
+                        <img src="../res/user.png" class="media-object" style="width:60px">
+                    </div>
+                    <div class="media-body">
+                        <h4 class="media-heading">User2 <small><i>19/02/2016 22:30</i></small></h4>
+                        <p>És uma merda</p>
+                    </div>
+                </div>
+                    <div class="media">
+                        <div class="media-left">
+                            <img src="../res/user.png" class="media-object" style="width:60px">
+                        </div>
+                        <div class="media-body">
+                            <h4 class="media-heading">User2 <small><i>19/02/2016 22:46</i></small></h4>
+                            <p>Mesmo Caralho</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+			<br>
+			<br>	
+			<div id="post3" class="row">
                 <div class="col-xs-1"><img src="../res/user.png" class="img-responsive"style="width:60px"></div>
                 <div class="col-xs-11">
                     <h4>User1 <small><i>Posted on February 19, 2016</i></small></h4>
