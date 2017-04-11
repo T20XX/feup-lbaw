709946b6987881c92619bcf65fbface7dbcc93e5
JoaoMoranguinho <joao.moranguinho13@gmail.com>
2017-04-11 03:15:39 +0100

    Javascript adicionado
    Path alterada.

---
+++ proto/javascript/circle.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $("#btn2 button").click(function(){
+        if($("#search_bar").css("display") == "none"){
+			$("#search_bar").css("display" , "block")
+		}
+		else{
+			$("#search_bar").css("display" , "none");
+		}
+    });
+});
+
+function open_left_sidebar() {
+    $('.left_sidebar').each(function () {
+        this.style.setProperty("display", "block", "important");
+    });
+}
+
+function close_left_sidebar() {
+    $('.left_sidebar').each(function () {
+        this.style.setProperty("display", "none");
+    });
+}
+
+function open_right_sidebar() {
+    $('.right_sidebar').each(function () {
+        this.style.setProperty("display", "block", "important");
+    });
+}
+
+function close_right_sidebar() {
+    $('.right_sidebar').each(function () {
+        this.style.setProperty("display", "none");
+    });
+}
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+});
--- proto/pages/auth/index.php
+++ proto/pages/auth/index.php
@@ -12,7 +12,7 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<script src="../js/homepage.js"></script>
+<script src="../../javascript/homepage.js"></script>
 
 <head>
     <style>
