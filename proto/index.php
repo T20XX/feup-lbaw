0e5cecb9a312c0971682a7faa390178c3f1c8bd6
armysick <gustpint@gmail.com>
2017-04-11 19:09:05 +0100

    checking session start (to complete)

--- proto/index.php
+++ proto/index.php
@@ -1,3 +1,9 @@
 <?php
-  header('Location: pages/auth');
+    //include_once("database/connection.php");
+    session_start();
+    if(isset($_SESSION['id_user'])){
+		header('Location: pages/auth');
+    } else{
+		header('Location: pages/auth');
+    }
 ?>
