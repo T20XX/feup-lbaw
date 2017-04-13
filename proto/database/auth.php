46e310db0ba21aa0dc2550b4a09b5dac5dc88fe3
Telmo Barros <telmo.20@hotmail.com>
2017-04-13 16:57:28 +0100

    auth try

--- proto/database/auth.php
+++ proto/database/auth.php
@@ -2,10 +2,10 @@
   
   function createUser($first_name, $last_name, $email, $password) { //TODO
     global $conn;
-    $stmt = $conn->prepare("INSERT INTO Person (email,password) VALUES (?, ?)");
+    $stmt = $conn->prepare("INSERT INTO 'Person' (email,password) VALUES (?, ?)");
     $stmt->execute(array($email, sha1($password)));
       $idPerson = $stmt->fetch();
-      $stmt = $conn->prepare("INSERT INTO User (idPerson, first_name, last_name) VALUES (?,?,?)");
+      $stmt = $conn->prepare("INSERT INTO 'User' (idPerson, first_name, last_name) VALUES (?,?,?)");
       $stmt->execute(array($idPerson, $first_name, $last_name));
 
   }
@@ -13,7 +13,7 @@
   function isLoginCorrect($email, $password) { //TODO verificacao password com hash
     global $conn;
     $stmt = $conn->prepare("SELECT idPerson 
-                            FROM Person 
+                            FROM 'Person' 
                             WHERE email = ? AND password = ?");
     $stmt->execute(array($email, sha1($password)));
     return $stmt->fetch() == true;
@@ -22,12 +22,12 @@
   function isAdmin($email) {
     global $conn;
     $stmt = $conn->prepare("SELECT idPerson 
-                            FROM Person 
+                            FROM 'Person' 
                             WHERE email = ?");
     $stmt->execute(array($email));
     $idPerson = $stmt->fetch();
     $stmt = $conn->prepare("SELECT * 
-                            FROM Admin 
+                            FROM 'Admin' 
                             WHERE idPerson = ?");
     $stmt->execute(array($idPerson));
     return $stmt->fetch() == true;
