e5d4e99d8284cbbb67b31a8e968f1439948ac903
Grupo 32 de LBAW <lbaw1632@gnomo.fe.up.pt>
2017-04-15 18:47:38 +0100

    Done admin landing page

--- proto/pages/admin/index.php
+++ proto/pages/admin/index.php
@@ -1,6 +1,6 @@
 <?php
 include_once('../../config/init.php');
-if(isset($_SESSION['admin'] && $_SESSION['admin'] != -1)){
+if(isset($_SESSION['admin']) && $_SESSION['admin'] != -1){
     header("Location: $BASE_URL" . 'pages/admin/feed.php');
 } else{
     header("Location: $BASE_URL" . 'pages/auth/');
