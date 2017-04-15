4750839dfeedaea74ac36e51bd5dfb8045a06741
Telmo Barros <telmo.20@hotmail.com>
2017-04-15 17:20:58 +0100

    profile and edit profile teste

--- proto/javascript/edit_profile.js
+++ proto/javascript/edit_profile.js
@@ -1,7 +1,16 @@
 console.log("tou a correr");
 $(document).ready(function () {
     $('#show_hometown').click(function () {
-        console.log("tou a correr");
         $(this).find('span').toggleClass('glyphicon-eye-close').toggleClass('glyphicon-eye-open');
+        toogleBooleanInput($(this).find('input'))
     });
 });
+
+function toogleBooleanInput(input){
+    var bool = $(input).val();
+    if(bool == "1"){
+        $(input).val(0);
+    }else if (bool == "0"){
+        $(input).val(1);
+    }
+}
