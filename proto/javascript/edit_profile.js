015456b15d64ac63f29b60a569b6ff1da6b0c3e9
Telmo Barros <telmo.20@hotmail.com>
2017-04-15 16:15:03 +0100

    profile and edit profile teste

--- proto/javascript/edit_profile.js
+++ proto/javascript/edit_profile.js
@@ -1,3 +1,5 @@
+console.log("tou a correr");
 $('#show_hometown').click(function(){
+    console.log("tou a correr");
     $(this).find('span').toggleClass('glyphicon-eye-close').toggleClass('glyphicon-eye-open');
 });
