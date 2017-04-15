eda91825183f047e953f7671b7047a1de09e5206
armysick <uh4v3b33nh4ck3d@gmail.com>
2017-04-15 18:24:40 +0100

    fixes

--- proto/pages/admin/feed.php
+++ proto/pages/admin/feed.php
@@ -1,14 +1,8 @@
 <?php
 include_once('../../config/init.php');
 
-if(isset($_SESSION['id'])){
-    if(!isset($_SESSION['admin'])){
-        $id = $_SESSION['id'];
-    }else {
-        header("Location: $BASE_URL" . 'pages/admin/');
-    }
-} else {
-    header("Location: $BASE_URL" . 'pages/auth/');
+if(!isset($_SESSION['admin']) || $_SESSION['admin'] != -1 )
+	header(header("Location: $BASE_URL" . 'pages/auth/');
 }
 
 $smarty->assign('cssPath', $BASE_URL . "css/feed.css");
