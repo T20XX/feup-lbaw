6778eb3fce42d33a27a33d8162e8f40489e01608
Jorge Vale <jorge-vale1@hotmail.com>
2017-04-16 01:49:25 -0700

    test circle

--- proto/database/user.php
+++ proto/database/user.php
@@ -28,10 +28,11 @@ function getUserImage($id) {
 
 function getUserCircles($id) {
     global $conn;
-    $stmt = $conn->prepare('SELECT "idCircle", "public"."Image".path
+    $stmt = $conn->prepare('SELECT "idCircle", "public"."Circle".name, "public"."Image".path
                             FROM "public"."Ingresso" JOIN
+								"public"."Circle" USING("idCircle")
 								"public"."Image" USING("idCircle")
-                            WHERE "idUser" = ?');
+                            WHERE "Ingresso"."idUser" = ?');
     $stmt->execute(array($id));
     return $stmt->fetchAll();
 }
