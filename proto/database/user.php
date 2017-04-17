a7fff63b0e6426bc0de2e5d7b59bea4d8de3516e
Jorge Vale <jorge-vale1@hotmail.com>
2017-04-17 03:19:04 -0700

    invite testing

--- proto/database/user.php
+++ proto/database/user.php
@@ -44,10 +44,9 @@ function getUserCircles($id)
 function getUserInvites($id)
 {
     global $conn;
-    $stmt = $conn->prepare('SELECT "public"."Invite".sender, "public"."Circle".name, "public"."Image".path
-                            FROM (("public"."Invite" JOIN
-								"public"."Circle" USING("idCircle")) RIGHT JOIN
-								"public"."Image" USING("idCircle"))
+    $stmt = $conn->prepare('SELECT "public"."Invite".sender, "public"."Circle".name
+                            FROM "public"."Invite" JOIN
+								"public"."Circle" USING("idCircle")
                             WHERE "Invite"."receiver" = ?');
     $stmt->execute(array($id));
     return $stmt->fetchAll();
