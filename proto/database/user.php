3e26313b397d92e8add4c17db56c391c93436476
Telmo Barros <telmo.20@hotmail.com>
2017-04-21 17:12:48 +0100

    change schema

--- proto/database/user.php
+++ proto/database/user.php
@@ -3,8 +3,8 @@ function getUserInfo($id)
 {
     global $conn;
     $stmt = $conn->prepare('SELECT * 
-                            FROM "public"."User" 
-                            WHERE "idPerson" = ?');
+                            FROM User 
+                            WHERE idPerson = ?');
     $stmt->execute(array($id));
     return $stmt->fetch();
 }
@@ -13,8 +13,8 @@ function getUserEmail($id)
 {
     global $conn;
     $stmt = $conn->prepare('SELECT email 
-                            FROM "public"."Person" 
-                            WHERE "idPerson" = ?');
+                            FROM Person
+                            WHERE idPerson = ?');
     $stmt->execute(array($id));
     return $stmt->fetch();
 }
@@ -23,8 +23,8 @@ function getUserImage($id)
 {
     global $conn;
     $stmt = $conn->prepare('SELECT path 
-                            FROM "public"."Image" 
-                            WHERE "idUser" = ?');
+                            FROM Image 
+                            WHERE idUser = ?');
     $stmt->execute(array($id));
     return $stmt->fetch();
 }
@@ -32,11 +32,11 @@ function getUserImage($id)
 function getUserCircles($id)
 {
     global $conn;
-    $stmt = $conn->prepare('SELECT "idCircle", "public"."Circle".name, "public"."Image".path
-                            FROM (("public"."Ingresso" JOIN
-								"public"."Circle" USING("idCircle"))JOIN
-								"public"."Image" USING("idCircle"))
-                            WHERE "Ingresso"."idUser" = ?');
+    $stmt = $conn->prepare('SELECT idCircle, Circle.name, Image.path
+                            FROM ((Ingresso JOIN
+								Circle USING(idCircle))JOIN
+								Image USING(idCircle))
+                            WHERE Ingresso.idUser = ?');
     $stmt->execute(array($id));
     return $stmt->fetchAll();
 }
@@ -44,12 +44,12 @@ function getUserCircles($id)
 function getUserInvites($id)
 {
     global $conn;
-    $stmt = $conn->prepare('SELECT "public"."Invite".sender, "public"."Circle"."idCircle", "public"."Circle".name, "public"."User".first_name, "public"."User".last_name, "public"."Image".path
-                            FROM ((("public"."Invite" JOIN
-								"public"."Circle" USING("idCircle")) JOIN
-								"public"."User" ON("public"."User"."idPerson" = "public"."Invite".sender)) JOIN
-								"public"."Image" USING("idCircle"))
-                            WHERE "Invite"."receiver" = ?');
+    $stmt = $conn->prepare('SELECT Invite.sender, Circle.idCircle, Circle.name, "User".first_name, "User".last_name, Image.path
+                            FROM (((Invite JOIN
+								Circle USING(idCircle)) JOIN
+								"User" ON("User".idPerson = Invite.sender)) JOIN
+								Image USING(idCircle))
+                            WHERE Invite.receiver = ?');
     $stmt->execute(array($id));
     return $stmt->fetchAll();
 }
@@ -57,18 +57,18 @@ function getUserInvites($id)
 function updateUserInfo($id, $first_name, $last_name, $hometown, $birthday, $gender, $bio, $show_hometown, $show_birthday, $show_gender, $show_age)
 {
     global $conn;
-    $stmt = $conn->prepare('UPDATE "public"."User"
+    $stmt = $conn->prepare('UPDATE "User"
                             SET (first_name, last_name, hometown, birthday, gender, bio, show_hometown, show_birthday, show_gender, show_age) = (? , ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                            WHERE "idPerson" = ?');
+                            WHERE idPerson = ?');
     $stmt->execute(array($first_name, $last_name, $hometown, $birthday, $gender, $bio, $show_hometown, $show_birthday, $show_gender, $show_age, $id));
 }
 
 function updateUserImage($id, $profile_photo)
 {
     global $conn;
-    $stmt = $conn->prepare('UPDATE "public"."Image"
+    $stmt = $conn->prepare('UPDATE Image
                             SET path = ?
-                            WHERE "idUser" = ?');
+                            WHERE idUser = ?');
     $stmt->execute(array($profile_photo, $id));
 }
 ?>
