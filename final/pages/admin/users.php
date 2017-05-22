9ac663d67283f352380cb9f02352205e8535d2b0
JoaoMoranguinho <joao.moranguinho13@gmail.com>
2017-05-22 09:00:10 +0100

    ..

--- final/pages/admin/users.php
+++ final/pages/admin/users.php
@@ -12,10 +12,10 @@ if(isset($_SESSION['id'])){
     header("Location: $BASE_URL" . 'pages/auth/');
 }
 
-if($_GET['reported'] === true){
+if($_GET['reported'] == true){
 $users = fetchAllReportedUsers();
 }
-else if ($_GET['reported'] === false){
+else if ($_GET['reported'] == false){
 $users = fetchAllUsers();
 }
 
